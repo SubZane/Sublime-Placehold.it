acde32971d7f9aa11a95edf8896a1b418bd07a08
Andreas Norman <subzane@subzane.com>
2012-12-15 23:55:53 +0100

    version 1.1

--- Placeholdit.py
+++ Placeholdit.py
@@ -1,9 +1,26 @@
+#   ------------------------------------------------------------
+#   Sublime Placehold.it Plugin
+#   Version 1.1
+#   by Andreas Norman (@andreasnorman)
+#
+#   https://github.com/SubZane/Sublime-Placehold.it
+#
+#   ------------------------------------------------------------
 import sublime, sublime_plugin, urllib2, json, os, re
 
 SETTINGS = sublime.load_settings("Placeholdit.sublime-settings")
 BGCOLOR = SETTINGS.get('ph_bgcolor')
 if BGCOLOR == None:
     BGCOLOR = "0eafff"
+DEFAULT_SIZES = SETTINGS.get('ph_default_sizes')
+if DEFAULT_SIZES == None:
+    DEFAULT_SIZES = "600x400, 200x200, 800x600, 100x100"
+IMAGEPATH = SETTINGS.get('ph_imagepath')
+if IMAGEPATH == None:
+    IMAGEPATH = "images/"
+SAVELOCAL = SETTINGS.get('ph_localimages')
+if SAVELOCAL == None:
+    SAVELOCAL = False
 TEXTCOLOR = SETTINGS.get('ph_textcolor')
 if TEXTCOLOR == None:
     TEXTCOLOR = "ffffff"
@@ -15,35 +32,84 @@ if FORMAT == None:
     FORMAT = ".png"
 TEXT = SETTINGS.get('ph_text')
 if TEXT == None:
-    TEXT = "600x400"
+    TEXT = ""
+
+def load_default_sizes():
+    global DEFAULT_SIZES
+    all_sizes = [sizes.strip() for sizes in DEFAULT_SIZES.split(',')]
+
+    return all_sizes
+
+def get_current_path():
+    view = sublime.Window.active_view(sublime.active_window())
+    current_file = view.file_name()
+    index = current_file.rfind('/')
+    current_dir = current_file[:index]
+    return current_dir
 
 def insert_image(size):
-    global BGCOLOR, TEXTCOLOR, FORMAT, TEXT
+    global BGCOLOR, TEXTCOLOR, FORMAT, TEXT, IMAGEPATH, SAVELOCAL
     if TEXT == "":
-        imageurl = 'http://placehold.it/{0}/{1}/{2}{3}'.format(size, BGCOLOR, TEXTCOLOR, FORMAT)
+        imageurl = 'http://placehold.it/{0}/{1}/{2}{3}'.format(size, BGCOLOR, TEXTCOLOR, FORMAT, size)
     else:
         imageurl = 'http://placehold.it/{0}/{1}/{2}{3}&text={4}'.format(size, BGCOLOR, TEXTCOLOR, FORMAT, TEXT)
-    imagetag = '<img alt="{0}" src="{1}" />'.format(TEXT, imageurl)
+
+    if SAVELOCAL:
+        file_name = "placehold-{0}-{1}-{2}{3}".format(size, BGCOLOR, TEXTCOLOR, FORMAT)
+        path = get_current_path()
+        imagefile = urllib2.urlopen(imageurl)
+        localFile = open(path+"/"+IMAGEPATH+file_name, 'w+')
+        localFile.write(imagefile.read())
+        localFile.close()
+        insert_image_tag(IMAGEPATH+file_name)
+    else:
+        insert_image_tag(imageurl)
+
+def insert_custom_size():
+    all_sizes = load_default_sizes()
+    def on_enter(num):
+        insert_image(all_sizes[num])
+
+    sublime.active_window().show_quick_panel(all_sizes, on_enter)
+
+def insert_image_tag(selected_image):
+    imagetag = '<img alt="" src="{0}" />'.format(selected_image)
     view = sublime.active_window().active_view()
     edit = view.begin_edit()
     for region in view.sel():
         view.replace(edit, region, imagetag)
     view.end_edit(edit) 
 
+def insert_cached_images():
+    global IMAGEPATH, FORMAT
+    current_dir = get_current_path()
+
+    imagefiles = [os.path.join(root, name)
+        for root, dirs, files in os.walk(current_dir+"/"+IMAGEPATH)
+            for name in files
+                if name.startswith("placehold") and name.endswith(FORMAT)]
+
+    imagefiles = [w.replace(current_dir+"/", '') for w in imagefiles]
+
+    def on_enter(selected_image):
+        if selected_image != -1:
+            insert_image_tag(imagefiles[selected_image])
+    sublime.active_window().show_quick_panel(imagefiles, on_enter)
+
 class InsertImageCommand(sublime_plugin.TextCommand):
     global BGCOLOR, TEXTCOLOR, SIZE, FORMAT, TEXT
     def run(self, edit):
         seloptions = []
-        seloptions.append("Insert Default Image ({0})".format(SIZE))
-        seloptions.append("Insert Custom Sized Image")
+        seloptions.append("Insert default size ({0})".format(SIZE))
+        seloptions.append("Insert custom size")
+        seloptions.append("Insert cached images")
 
         def on_enter(num):
             if num == 0:
                 insert_image(SIZE)
             elif num == 1:
-                sublime.active_window().show_input_panel("Set Size:", "600x400", on_size, None, None)
-
-        def on_size(setsize):
-            insert_image(setsize)
+                insert_custom_size()
+            elif num == 2:
+                insert_cached_images()
 
         sublime.active_window().show_quick_panel(seloptions, on_enter)
--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-Sublime-Placehold.it
+Sublime-Placehold.it 1.1
 =============================
 
 Placehold.it plugin for Sublime Text 2
 
 A plugin using the features of [Placehold.it](http://placehold.it)
 * Can insert an image tag with dummy image
+* Can store images locally to improve development speed and limit requests
 * Can insert any sized image tag with dummy image
+* Can browse and insert locally stored placeholder.it images
 * Text, Colors and Size can all be configured in the settings
+
+## Changelog
+### Version 1.1
+* Can now store dummy images locally
+* Can browse locally stored images
+* Settings for adding a list of different custom sizes
+* Check the settings file for new settings
+* Better readme file
+
+### Version 1.0
+* Initial release
 
 ## Installation
 ### Package Control
@@ -27,11 +40,21 @@ Go to your Sublime Text 2 Packages directory and clone the repository using the 
 
 ## How to use
 * Launch the Command Palette using the menu (Tools->Command Palette...) or short key-command Shift+Cmd+P
-* Find Placehold.it Insert Image (for default sized image)
-* Find Placehold.it Insert Custom Image (to set your own size of the image)
+* Be sure to look through the settings before use
+* Find Placehold.it Insert default size (for default sized image)
+* Find Placehold.it Insert custom size (to list your own custom set sizes)
+* Find Placehold.it Insert cached images (to browse and insert cached images)
+
+## Settings
+Find the settings file in the menu: Sublime Text2 ~> Preferences ~> Package Settings ~> Snipt.net Snippet Fetcher ~> Settings
+* `ph_bgcolor` Change this to set your own background color (HEX value)
+* `ph_textcolor` Change this to set your own text color (HEX value)
+* `ph_size` The default size of the image, for quick insert
+* `ph_default_sizes` List of image sizes to insert
+* `ph_format` File format. You can use .png, .gif or .jpg
+* `ph_text` The text on the dummy image. Default is the size of the image
+* `ph_localimages` True/False if you want to save the dummy images locally
+* `ph_imagepath` The path for your image folder in your web project. This is where the dummy images is stored
 
 ### Using key-commands
 * p, h, tab (to insert image)
-
-##Todo's
-* Add support to download and store dummy image localy
