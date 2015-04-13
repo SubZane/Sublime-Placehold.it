e49626da3366d1cb21ffc20c6a617edaada0d160
Otec Fura <fura.otec@gmail.com>
2015-04-13 15:23:27 +0200

    convert to sublime text 3

--- Placeholdit.py
+++ Placeholdit.py
@@ -6,35 +6,49 @@
 #   https://github.com/SubZane/Sublime-Placehold.it
 #
 #   ------------------------------------------------------------
-import sublime, sublime_plugin, urllib2, json, os, re
+import sublime, sublime_plugin, json, os, re
+from urllib.request import urlopen
 
-SETTINGS = sublime.load_settings("Placeholdit.sublime-settings")
-BGCOLOR = SETTINGS.get('ph_bgcolor')
-if (BGCOLOR == ""):
-    BGCOLOR = "0eafff"
-DEFAULT_SIZES = SETTINGS.get('ph_default_sizes')
-if (DEFAULT_SIZES == ""):
-    DEFAULT_SIZES = "600x400, 200x200, 800x600, 100x100"
-IMAGEPATH = SETTINGS.get('ph_imagepath')
-if (IMAGEPATH == ""):
-    IMAGEPATH = "images/"
-TEXTCOLOR = SETTINGS.get('ph_textcolor')
-if (TEXTCOLOR == ""):
-    TEXTCOLOR = "ffffff"
-SIZE = SETTINGS.get('ph_size')
-if (SIZE == ""):
-    SIZE = "600x400"
-FORMAT = SETTINGS.get('ph_format')
-if (FORMAT == ""):
-    FORMAT = ".png"
-TEXT = SETTINGS.get('ph_text')
-if (TEXT == ""):
-    TEXT = ""
-SAVELOCAL = SETTINGS.get('ph_save_local')
-if SAVELOCAL == 0:
-    SAVELOCAL = False
-else:
-    SAVELOCAL = True
+SETTINGS = None
+BGCOLOR = None
+DEFAULT_SIZES = None
+IMAGEPATH = None
+TEXTCOLOR = None
+SIZE = None
+FORMAT = None
+TEXT = None
+SAVELOCAL = None
+REGION = None
+
+def loadVariables():
+    global SETTINGS, BGCOLOR, DEFAULT_SIZES, IMAGEPATH,TEXTCOLOR, SIZE, FORMAT, TEXT, SAVELOCAL,REGION
+    SETTINGS = sublime.load_settings("Placeholdit.sublime-settings")
+    BGCOLOR = SETTINGS.get('ph_bgcolor')
+    if (BGCOLOR == ""):
+        BGCOLOR = "0eafff"
+    DEFAULT_SIZES = SETTINGS.get('ph_default_sizes')
+    if (DEFAULT_SIZES == ""):
+        DEFAULT_SIZES = "600x400, 200x200, 800x600, 100x100"
+    IMAGEPATH = SETTINGS.get('ph_imagepath')
+    if (IMAGEPATH == ""):
+        IMAGEPATH = "images/"
+    TEXTCOLOR = SETTINGS.get('ph_textcolor')
+    if (TEXTCOLOR == ""):
+        TEXTCOLOR = "ffffff"
+    SIZE = SETTINGS.get('ph_size')
+    if (SIZE == ""):
+        SIZE = "600x400"
+    FORMAT = SETTINGS.get('ph_format')
+    if (FORMAT == ""):
+        FORMAT = ".png"
+    TEXT = SETTINGS.get('ph_text')
+    if (TEXT == ""):
+        TEXT = ""
+    SAVELOCAL = SETTINGS.get('ph_save_local')
+    if SAVELOCAL == 0:
+      SAVELOCAL = False
+    else:
+        SAVELOCAL = True
 
 
 def IsNull(value):
@@ -68,14 +82,18 @@ def insert_image(size):
         path = get_current_path()
         if path == False:
             return False
-        imagefile = urllib2.urlopen(imageurl)
+        imagefile = urlopen(imageurl)
         try:
-            localFile = open(path+"/"+IMAGEPATH+file_name, 'w+')
+            path_images = path+"/"+IMAGEPATH
+            if not os.path.exists(path_images):
+                os.makedirs(path_images)
+            localFile = open(path_images+file_name, 'w+')
             localFile.write(imagefile.read())
             localFile.close()
             insert_image_tag(IMAGEPATH+file_name)
-        except Exception, e:
-            print "Placehold.it Error: Folder not found. Unable to save image. Falling back to URL."
+        except Exception:
+            print("Placehold.it Error: Folder not found. Unable to save image. Falling back to URL.")
+
             insert_image_tag(imageurl)
     else:
         insert_image_tag(imageurl)
@@ -84,16 +102,20 @@ def insert_custom_size():
     all_sizes = load_default_sizes()
     def on_enter(num):
         insert_image(all_sizes[num])
+    show_quick_panel(all_sizes, on_enter)
 
-    sublime.active_window().show_quick_panel(all_sizes, on_enter)
+
+
+class ReplaceCommand(sublime_plugin.TextCommand):
+    def run(self, edit, imagetag):
+        global REGION
+        self.view.replace(edit, REGION, imagetag)
+
 
 def insert_image_tag(selected_image):
     imagetag = '<img alt="" src="{0}" />'.format(selected_image)
     view = sublime.active_window().active_view()
-    edit = view.begin_edit()
-    for region in view.sel():
-        view.replace(edit, region, imagetag)
-    view.end_edit(edit) 
+    view.run_command("replace", {'imagetag':imagetag})
 
 def insert_cached_images():
     global IMAGEPATH, FORMAT
@@ -108,11 +130,20 @@ def insert_cached_images():
     def on_enter(selected_image):
         if selected_image != -1:
             insert_image_tag(imagefiles[selected_image])
-    sublime.active_window().show_quick_panel(imagefiles, on_enter)
+    show_quick_panel(imagefiles, on_enter)
+
+def show_quick_panel(options, done):
+        sublime.set_timeout(lambda: sublime.active_window().show_quick_panel(options, done), 10)
 
 class InsertImageCommand(sublime_plugin.TextCommand):
     global BGCOLOR, TEXTCOLOR, SIZE, FORMAT, TEXT
+
     def run(self, edit):
+        loadVariables()
+        global REGION
+        view = self.view
+        REGION=view.sel();
+        REGION=REGION[0];
         seloptions = []
         seloptions.append("Insert default size ({0})".format(SIZE))
         seloptions.append("Insert custom size")
@@ -125,5 +156,4 @@ class InsertImageCommand(sublime_plugin.TextCommand):
                 insert_custom_size()
             elif num == 2:
                 insert_cached_images()
-
-        sublime.active_window().show_quick_panel(seloptions, on_enter)
+        show_quick_panel(seloptions, on_enter)
